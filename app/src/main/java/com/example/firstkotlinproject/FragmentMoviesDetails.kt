8b096e9f79f7c5afd5f2af2b02643779cb37f872
Ynikum <alexfalatyuk2@gmail.com>
2021-09-04 13:42:09 +0300

    1.Created fragment_movies_details.xml 2.Moved layout from activity_details.xml to fragment_movies_details.xml 3.Created activity_main.xml and add FrameLayout to it 4. Created FragmentMoviesDetails.kt and inflate fragment_movies_details.xml in it 5. Created fragment_movies_list.xml and added some design

---
+++ app/src/main/java/com/example/firstkotlinproject/FragmentMoviesDetails.kt
@@ -0,0 +1,12 @@
+package com.example.firstkotlinproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
+
+}
--- app/src/main/java/com/example/firstkotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinproject/MainActivity.kt
@@ -12,11 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tvMain: TextView = findViewById(R.id.tvMain)
-        tvMain.setOnClickListener{moveToNextActivity()}
+        
     }
-    private fun moveToNextActivity(){
-        val intent = Intent(this,MovieDetailsActivity::class.java)
-        startActivity(intent)
-    }
+
 }
