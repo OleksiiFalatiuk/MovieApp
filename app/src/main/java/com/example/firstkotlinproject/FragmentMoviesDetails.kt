c501f5a79c11c77f9bd161041d986c6f5623ab47
Ynikum <alexfalatyuk98@gmail.com>
2021-10-18 21:25:43 +0300

    updated some code in MovieDetailAdapter.kt

--- app/src/main/java/com/example/firstkotlinproject/FragmentMoviesDetails.kt
+++ app/src/main/java/com/example/firstkotlinproject/FragmentMoviesDetails.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.os.bundleOf
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.firstkotlinproject.adapters.MovieDetailAdapter
+import com.example.firstkotlinproject.data.Movie
 
 
 class FragmentMoviesDetails : Fragment() {
@@ -23,18 +28,64 @@ class FragmentMoviesDetails : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        recyclerDetail =  view.findViewById(R.id.rvDetail)
-        recyclerDetail?.adapter = MovieDetailAdapter()
+        val movieData = arguments?.getSerializable(PARAM_MOVIE_DATA) as? Movie ?: return
+
+//        updateMovieDetailsInfo(movieData)
+
+        view.findViewById<RecyclerView>(R.id.recycler_movies).apply {
+
+            this.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+
+            val adapter = MovieDetailAdapter()
+
+            this.adapter = adapter
+
+            adapter.submitList(movieData.actors)
+        }
+
     }
 
-    override fun onStart() {
-        updateData()
-        super.onStart()
+    private fun updateMovieDetailsInfo(movie : Movie) {
+        view?.findViewById<ImageView>(R.id.imageDetail)
+            ?.setImageResource(movie.detailImageRes)
+
+        view?.findViewById<TextView>(R.id.yearsDetail)?.text =
+            context?.getString(R.string.movies_list_allowed_age_template, movie.pgAge)
+
+        view?.findViewById<TextView>(R.id.movie_name_text)?.text = movie.title
+        view?.findViewById<TextView>(R.id.movie_tags_text)?.text = movie.genre
+        view?.findViewById<TextView>(R.id.movie_reviews_count_text)?.text =
+            context?.getString(R.string.movies_list_reviews_template, movie.reviewCount)
+        view?.findViewById<TextView>(R.id.movie_storyline_text)?.text = movie.storyLine
+
+        val starsImages = listOf<ImageView?>(
+            view?.findViewById(R.id.star1_image),
+            view?.findViewById(R.id.star2_image),
+            view?.findViewById(R.id.star3_image),
+            view?.findViewById(R.id.star4_image),
+            view?.findViewById(R.id.star5_image)
+        )
     }
 
-    private fun updateData() {
-        (recyclerDetail?.adapter as? MovieDetailAdapter)?.apply {
-            bindActorsDetail(DetailData().getDetail())
+//    override fun onStart() {
+//        updateData()
+//        super.onStart()
+//    }
+//
+//    private fun updateData() {
+//        (recyclerDetail?.adapter as? MovieDetailAdapter)?.apply {
+//            bindActorsDetail(DetailData().getDetail())
+//        }
+//    }
+
+    companion object {
+        private const val PARAM_MOVIE_DATA = "movie_data"
+
+        fun create(movie : Movie) = FragmentMoviesDetails().also {
+            val args = bundleOf(
+                PARAM_MOVIE_DATA to movie
+            )
+            it.arguments = args
         }
     }
 
--- app/src/main/java/com/example/firstkotlinproject/adapters/MovieDetailAdapter.kt
+++ app/src/main/java/com/example/firstkotlinproject/adapters/MovieDetailAdapter.kt
@@ -6,72 +6,45 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.firstkotlinproject.R
 import com.example.firstkotlinproject.data.ActorData
 
-class MovieDetailAdapter: RecyclerView.Adapter<MovieDetailAdapter.ViewHolder>() {
-
-    private var listDetail = listOf<ActorData>()
+class MovieDetailAdapter: ListAdapter<ActorData, MovieDetailAdapter.ViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.view_holder_actor, parent, false)
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.view_holder_actor, parent, false)
         )
     }
 
-    override fun onBindViewHolder(holder: MovieDetailAdapter.ViewHolder, position: Int) {
-        val detail = listDetail[position]
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            holder.imageDetail?.setImageDrawable(holder.itemView.context.getDrawable(detail.imageDetail))
-            holder.detailName?.text = detail.detailName
-            holder.detailAge?.text = detail.detailAge
-            holder.tag?.text = detail.tag
-            holder.detailStar1?.setImageDrawable(holder.itemView.context.getDrawable(detail.detailStar1))
-            holder.detailStar2?.setImageDrawable(holder.itemView.context.getDrawable(detail.detailStar2))
-            holder.detailStar3?.setImageDrawable(holder.itemView.context.getDrawable(detail.detailStar3))
-            holder.detailStar4?.setImageDrawable(holder.itemView.context.getDrawable(detail.detailStar4))
-            holder.detailStar5?.setImageDrawable(holder.itemView.context.getDrawable(detail.detailStar5))
-            holder.detailReviews?.text = detail.detailReviews
-            holder.detailStory?.text = detail.story
-            holder.actor1?.setImageDrawable(holder.itemView.context.getDrawable(detail.actor1))
-            holder.actor2?.setImageDrawable(holder.itemView.context.getDrawable(detail.actor2))
-            holder.actor3?.setImageDrawable(holder.itemView.context.getDrawable(detail.actor3))
-            holder.actor4?.setImageDrawable(holder.itemView.context.getDrawable(detail.actor4))
-            holder.nameActor1?.text = detail.nameActor1
-            holder.nameActor2?.text = detail.nameActor2
-            holder.nameActor3?.text = detail.nameActor3
-            holder.nameActor4?.text = detail.nameActor4
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val actorImage: ImageView = itemView.findViewById(R.id.actor_image)
+        private val actorName: TextView = itemView.findViewById(R.id.actor_name)
+
+        fun bind(item: ActorData) {
+            actorImage.setImageResource(item.imageRes)
+            actorName.text = item.name
         }
     }
 
-    fun bindActorsDetail(newDetail: List<ActorData>) {
-        listDetail = newDetail
-        notifyDataSetChanged()
-    }
+    class DiffCallback : DiffUtil.ItemCallback<ActorData>() {
+        override fun areItemsTheSame(oldItem: ActorData, newItem: ActorData): Boolean {
+            return oldItem.id == newItem.id
+        }
 
-    override fun getItemCount(): Int = listDetail.size
-
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
-        val imageDetail: ImageView? = itemView.findViewById(R.id.imageDetail)
-        val detailName: TextView? = itemView.findViewById(R.id.detailName)
-        val detailAge: TextView? = itemView.findViewById(R.id.yearsDetail)
-        val tag: TextView? = itemView.findViewById(R.id.tag)
-        val detailStar1: ImageView? = itemView.findViewById(R.id.detailStar1)
-        val detailStar2: ImageView? = itemView.findViewById(R.id.detailStar2)
-        val detailStar3: ImageView? = itemView.findViewById(R.id.detailStar3)
-        val detailStar4: ImageView? = itemView.findViewById(R.id.detailStar4)
-        val detailStar5: ImageView? = itemView.findViewById(R.id.detailStar5)
-        val detailReviews: TextView? = itemView.findViewById(R.id.detailReviews)
-        val detailStory: TextView? = itemView.findViewById(R.id.detailStory)
-        val actor1: ImageView? = itemView.findViewById(R.id.ivDetail1)
-        val actor2: ImageView? = itemView.findViewById(R.id.ivDetail2)
-        val actor3: ImageView? = itemView.findViewById(R.id.ivDetail3)
-        val actor4: ImageView? = itemView.findViewById(R.id.ivDetail4)
-        val nameActor1: TextView? = itemView.findViewById(R.id.tvDetail1)
-        val nameActor2: TextView? = itemView.findViewById(R.id.tvDetail2)
-        val nameActor3: TextView? = itemView.findViewById(R.id.tvDetail3)
-        val nameActor4: TextView? = itemView.findViewById(R.id.tvDetail4)
-
+        override fun areContentsTheSame(oldItem: ActorData, newItem: ActorData): Boolean {
+            return oldItem == newItem
+        }
     }
 }
